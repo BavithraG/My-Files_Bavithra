15b8f7c163bc89cf5ea608f0bed1313df9fe013e
BavithraG <80410283+BavithraG@users.noreply.github.com>
2021-03-11 09:09:40 +0530

    FavouriteFoods.java

---
+++ FavouriteFoods.java
@@ -0,0 +1,8 @@
+package com.mycompany.myjavaprograms;
+
+class FavouriteFoods {
+    public static void main(String[] args) {
+        System.out.println("\t\t\tFAVOURITE FOODS\n \t\t\t---------------\nPav Bhaji\nChicken 65\nIce Cream\nCakes\nNoodles");
+        
+    }
+}
