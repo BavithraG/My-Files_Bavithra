2996b84bdc074ed405271894534c85a58889e376
BavithraG <80410283+BavithraG@users.noreply.github.com>
2021-03-11 23:10:16 +0530

    OperatorDemo

---
+++ OperatorDemo.java
@@ -0,0 +1,32 @@
+cd C:\Users\DELL\Documents\NetBeansProjects\MyJavaPrograms; "JAVA_HOME=C:\\Program Files\\Java\\jdk-15.0.2" cmd /c "\"C:\\Program Files\\NetBeans-12.3\\netbeans\\java\\maven\\bin\\mvn.cmd\" -Dexec.args=\"-classpath %classpath OperatorDemo\" -Dexec.executable=\"C:\\Program Files\\Java\\jdk-15.0.2\\bin\\java.exe\" -Dexec.classpathScope=runtime -Dmaven.ext.class.path=\"C:\\Program Files\\NetBeans-12.3\\netbeans\\java\\maven-nblib\\netbeans-eventspy.jar\" -Dfile.encoding=UTF-8 process-classes org.codehaus.mojo:exec-maven-plugin:3.0.0:exec"
+Scanning for projects...
+
+--------------------< com.mycompany:MyJavaPrograms >--------------------
+Building MyJavaPrograms 1.0-SNAPSHOT
+--------------------------------[ jar ]---------------------------------
+
+--- maven-resources-plugin:2.6:resources (default-resources) @ MyJavaPrograms ---
+Using 'UTF-8' encoding to copy filtered resources.
+skip non existing resourceDirectory C:\Users\DELL\Documents\NetBeansProjects\MyJavaPrograms\src\main\resources
+
+--- maven-compiler-plugin:3.1:compile (default-compile) @ MyJavaPrograms ---
+Changes detected - recompiling the module!
+Compiling 2 source files to C:\Users\DELL\Documents\NetBeansProjects\MyJavaPrograms\target\classes
+
+--- exec-maven-plugin:3.0.0:exec (default-cli) @ MyJavaPrograms ---
+10
+10
+10
+10
+10
+10
+10
+10
+10
+10
+------------------------------------------------------------------------
+BUILD SUCCESS
+------------------------------------------------------------------------
+Total time:  2.422 s
+Finished at: 2021-03-11T23:03:53+05:30
+------------------------------------------------------------------------
