383ec5bc18d90ece3b260c0d8b3a548fd6552758
BavithraG <80410283+BavithraG@users.noreply.github.com>
2021-03-11 23:24:36 +0530

    OperatorDemo

---
+++ OperatorDemo.java
@@ -0,0 +1,16 @@
+
+public class OperatorDemo {
+    public static void main(String[] args){
+        int a=0, b=1, c=2, d=3, e=4, f=5, g=6, h=7, i=8, j=9;
+        System.out.println(a+=10);
+        System.out.println(b+=9);
+        System.out.println(c+=8);
+        System.out.println(d+=7);
+        System.out.println(e+=6);
+        System.out.println(f+=5);
+        System.out.println(g+=4);
+        System.out.println(h+=3);
+        System.out.println(i+=2);
+        System.out.println(j+=1);
+    }
+}
