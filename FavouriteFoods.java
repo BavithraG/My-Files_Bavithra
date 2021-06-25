10e4cf28eed9e00f92c3ef0e184498e9b5b807f5
BavithraG <80410283+BavithraG@users.noreply.github.com>
2021-06-25 11:10:40 +0530

    Delete FavouriteFoods.java

--- FavouriteFoods.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.myjavaprograms;
-
-class FavouriteFoods {
-    public static void main(String[] args) {
-        System.out.println("\t\t\tFAVOURITE FOODS\n \t\t\t---------------\nPav Bhaji\nChicken 65\nIce Cream\nCakes\nNoodles");
-        
-    }
-}
