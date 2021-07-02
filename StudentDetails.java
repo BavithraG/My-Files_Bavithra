9f7407a823082edd683372d1b88849a377ec56a8
BavithraG <80410283+BavithraG@users.noreply.github.com>
2021-07-02 18:05:50 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,20 @@
+public class StudentDetails {
+    public static void main(String[] args){
+        int M1=100, M2=79, M3=78, M4=88, M5=98, age=24, rollno=10;
+        int result = M1+M2+M3+M4+M5;
+        int average=result/5;
+        String name = "Bavithra";
+        String mailID= "bavithra.ganesh97@gmail.com";
+        System.out.println("STUDENT DETAILS");
+        System.out.println("---------------");
+        System.out.println("Name: "+name);
+        System.out.println("Age :" +age);
+        System.out.println("Roll No: "+rollno);
+        System.out.println("Mail ID: "+mailID);
+        System.out.println("Result: "+result);
+        System.out.println("Average: "+average);
+        
+        
+    }
+    
+}
